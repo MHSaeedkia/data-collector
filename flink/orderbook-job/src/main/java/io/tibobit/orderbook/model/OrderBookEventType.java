ce3b87d2e2f413635a7c68f677b8b166fb73ddad
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2026-06-28 16:17:44 +0330

    fix data serialization

--- flink/orderbook-job/src/main/java/io/tibobit/orderbook/model/OrderBookEventType.java
+++ flink/orderbook-job/src/main/java/io/tibobit/orderbook/model/OrderBookEventType.java
@@ -1,5 +1,8 @@
 package io.tibobit.orderbook.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum OrderBookEventType {
     UPDATE("update"),
     SNAPSHOT("snapshot");
@@ -10,11 +13,13 @@ public enum OrderBookEventType {
         this.value = value;
     }
 
+    @JsonValue
     public String getValue() {
         return value;
     }
 
     // Optional: convert from string back to enum
+    @JsonCreator
     public static OrderBookEventType fromValue(String value) {
         for (OrderBookEventType type : OrderBookEventType.values()) {
             if (type.value.equalsIgnoreCase(value)) {
